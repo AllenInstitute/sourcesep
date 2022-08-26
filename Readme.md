6b2196dac5032c1e7f42da9f288e99bcac27e912
Rohan <rhngla@gmail.com>
2022-08-26 14:22:19 -0700

    h5 io works

---
+++ Readme.md
@@ -0,0 +1,15 @@
+### Description
+
+Source separation for multiplexed modulator imaging.
+
+### Environment
+```
+conda activate sourcesep python=3.8
+conda install scikit-learn statsmodels jupyterlab pandas seaborn scipy rich tqdm autopep8 h5py pytables
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch
+pip install timebudget
+pip install -e .
+```
+
+### Contributors:
+Rohan Gala, Smrithi Sunil, Kaspar Podgorski, Uygar Sümbül
---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("Readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+print("====Listing packages found:====")
+print(setuptools.find_packages())
+print("===============================")
+setuptools.setup(
+    name="sourcesep",
+    description="Source separation for multiplexed modulator imaging",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
