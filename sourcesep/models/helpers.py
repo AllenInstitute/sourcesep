392f293a74f6b6dc3d9662ce6bb6e14e418c9e6d
Rohan <rhngla@gmail.com>
2023-04-12 15:59:41 -0700

    refactored dataloaders to match lightning api

--- sourcesep/models/helpers.py
+++ sourcesep/models/helpers.py
@@ -7,7 +7,8 @@ from pathlib import Path
 
 
 class H5Dataset(Dataset):
-    def __init__(self, h5_filename, n_per_sample):
+    def __init__(self, h5_filename, n_timesamples):
+        self.n_timesamples = n_timesamples
         self.f = h5py.File(h5_filename, "r")
         cfg_filename = h5_filename.replace('.h5', '.toml')
         if Path.exists(Path(cfg_filename)):
@@ -15,13 +16,12 @@ class H5Dataset(Dataset):
         else:
             print(f'{cfg_filename} not found!')
             self.cfg = None
-        self.n_per_sample = n_per_sample
 
     def __len__(self):
-        return self.f['O'].shape[0] - self.n_per_sample
-    
+        return self.f['O'].shape[0] - self.n_timesamples
+
     def _proc(self, x, idx):
-        return np.swapaxes(x[idx:idx+self.n_per_sample].reshape(self.n_per_sample, -1),axis1=0,axis2=1).astype(np.float32)
+        return np.swapaxes(x[idx:idx+self.n_timesamples].reshape(self.n_timesamples, -1), axis1=0, axis2=1).astype(np.float32)
 
     def __getitem__(self, idx):
         return dict(O=self._proc(self.f['O'], idx),
@@ -33,57 +33,61 @@ class H5Dataset(Dataset):
 
 
 class H5DataModule(pl.LightningDataModule):
-    def __init__(self, h5_filename, n_per_sample, train_steps_per_epoch=400, val_steps_per_epoch=20, batch_size=20, num_workers=8):
+    def __init__(self, train_h5_filename,
+                 val_h5_filename,
+                 n_timesamples,
+                 train_steps_per_epoch=400,
+                 val_steps_per_epoch=20,
+                 validation_seed=None,
+                 batch_size=20,
+                 num_workers=8):
         super().__init__()
-        self.h5_filename = h5_filename
-        self.n_per_sample = n_per_sample
+        self.train_h5_filename = train_h5_filename
+        self.val_h5_filename = val_h5_filename
+        self.n_timesamples = n_timesamples
         self.batch_size = batch_size
         self.train_steps_per_epoch = train_steps_per_epoch
         self.val_steps_per_epoch = val_steps_per_epoch
         self.dataset = None
-        self.val_sub_dataloader = None
+        self.validation_seed = validation_seed
         self.num_workers = num_workers
 
     def get_sim_arrays(self):
         f = h5py.File(self.h5_filename, "r")
         S = f['S'][:]
-        W = f['W'][:] 
-        E = f['E'][:] 
+        W = f['W'][:]
+        E = f['E'][:]
         Mu_ox = f['Mu_ox'][:]
         Mu_dox = f['Mu_dox'][:]
         B = f['B'][:]
         return S, W, E, Mu_ox, Mu_dox, B
 
     def setup(self, stage: str):
-        # Assign train/val datasets for use in dataloaders
         if stage == "fit":
-            self.dataset = H5Dataset(h5_filename=self.h5_filename, n_per_sample=self.n_per_sample)
-
-            # validation remains the same across epochs
-            val_subset_idx = np.random.choice(len(self.dataset), self.val_steps_per_epoch, replace=False)
-            val_sub_dataset = Subset(self.dataset, val_subset_idx)
-            self.val_sub_dataloader = DataLoader(val_sub_dataset, batch_size=self.batch_size, 
-                                                shuffle=False, num_workers=8, pin_memory=True)
-
-        if stage == "test":
-            # use when ground truth available
-            pass
-
-        if stage == "predict":
-            # use when ground truth absent
-            pass
+            self.train_dataset = H5Dataset(h5_filename=self.train_h5_filename,
+                                           n_timesamples=self.n_timesamples)
+            self.val_dataset = H5Dataset(h5_filename=self.val_h5_filename,
+                                         n_timesamples=self.n_timesamples)
 
     def train_dataloader(self):
-        subset_idx = np.random.choice(len(self.dataset), self.train_steps_per_epoch, replace=False)
+        subset_idx = np.random.choice(a=len(self.dataset),
+                                      size=self.train_steps_per_epoch,
+                                      replace=False)
         sub_dataset = Subset(self.dataset, subset_idx)
-        return DataLoader(sub_dataset, batch_size=self.batch_size, 
-                          shuffle=True, num_workers=self.num_workers, pin_memory=True)
+        return DataLoader(sub_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=True,
+                          num_workers=self.num_workers,
+                          pin_memory=True)
 
     def val_dataloader(self):
-        return self.val_sub_dataloader
-
-    def test_dataloader(self):
-        pass
-
-    def predict_dataloader(self):
-        pass
+        rng = np.random.RandomState(self.validation_seed)
+        subset_idx = rng.random.choice(a=len(self.dataset),
+                                       size=self.val_steps_per_epoch,
+                                       replace=False)
+        sub_dataset = Subset(self.dataset, subset_idx)
+        return DataLoader(sub_dataset,
+                          batch_size=self.batch_size,
+                          shuffle=True,
+                          num_workers=self.num_workers,
+                          pin_memory=True)
